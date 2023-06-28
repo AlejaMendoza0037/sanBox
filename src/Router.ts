46e1acc75d77843a5961e40c63c857a57d7d691f
Aleja Mendoza <alejamendoza0@gmail.com>
2023-06-28 13:54:52 -0500

    version 0.0.2.2 comfiguracion de slint

--- src/Router.ts
+++ src/Router.ts
@@ -4,58 +4,68 @@ import swaggerUi from 'swagger-ui-express'
 import { swaggerSpec } from './swagger.conf'
 import express,{Application, Request, Response} from 'express'
 
-
+/**
+ * Clase principal de la API: define las turas de la API
+ * 
+ * @autor Alejandra Mendoza
+ * @description rutas y documentacion
+ */
 
 class App{
-    //atributo
+	//atributo
 
-    public app:Application
-    private server: any// prender y apagar el servidor que se ejecuta
+	public app:Application
+	private server:any// prender y apagar el servidor que se ejecuta
+    
+	constructor(){
 
-    constructor(){
-        this.app=express()
-        this.app.use(express.json())
-        this.app.use(
-            '/api-docs',
+		/**
+         * Expres es la biblioteca para definir API  en el 
+         * ecosistema express */
+   
+		this.app=express()
+		this.app.use(express.json())
+		this.app.use(
+			'/api-docs',
            
-            swaggerUi.serve,
-            swaggerUi.setup(swaggerSpec)
+			swaggerUi.serve,
+			swaggerUi.setup(swaggerSpec)
             
             
-        )
+		)
 
-        this.routes()  //definimos rutas, definir puntos de entrada al proyecto      
-    }
+		this.routes()  //definimos rutas, definir puntos de entrada al proyecto      
+	}
 
-    private routes():void{// solo se puede acceder desde esta clase 
-        this.app.get(
-            '/',//url
-            (req:Request, res:Response)=>{
-                res.send("Bienvenidos a la IPS")//send enviar
-            }
-        )
+	private routes():void{// solo se puede acceder desde esta clase 
+		this.app.get(
+			'/',//url
+			(req:Request, res:Response)=>{
+				res.send('Bienvenidos a la IPS")//send enviar')
+			}
+		)
 
-        this.app.post(
-            "/paciente",//url
-            (req:Request, res:Response)=>{
-                res.send("Bienvenidos a TypeScript")//send enviar
-            }
-        )
-        }
+		this.app.post(
+			'/paciente',//url
+			(req:Request, res:Response)=>{
+				res.send('Bienvenidos a TypeScript')//send enviar
+			}
+		)
+	}
 
-    public star():void{
-        this.server=this.app.listen(
-            3000,
-            ()=>{console.log("El servidor esta escuchando en el puerto 3000")}
+	public star():void{
+		this.server=this.app.listen(
+			3000,
+			()=>{console.log('El servidor esta escuchando en el puerto 3000')}
 
-        )
+		)
 
-        }
+	}
 
-    public  close():void{ //cerrar el servidor
-        this.server.close()
+	public  close():void{ //cerrar el servidor
+		this.server.close()
 
 
-    }    
+	}    
 }
 export default App
